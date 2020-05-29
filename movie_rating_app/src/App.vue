b4b93857c5795a7cf4e7e6eb74ff25b9f1d41f99
SamuelEdward <samueledward40@gmail.com>
2020-05-29 09:38:12 -0700

    Segregating Css

--- movie_rating_app/src/App.vue
+++ movie_rating_app/src/App.vue
@@ -10,14 +10,8 @@ export default {
   name: 'App'
 }
 </script>
+import './assets/stylesheets/main.css';
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
