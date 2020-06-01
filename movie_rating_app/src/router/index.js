98803a6d9ee103adc351428ac92d12eb122cd343
SamuelEdward <samueledward40@gmail.com>
2020-06-01 06:07:51 -0700

    setting contact route

--- movie_rating_app/src/router/index.js
+++ movie_rating_app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Contact from '@/components/Contact'
 
 Vue.use(Router)
 
@@ -10,7 +11,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact,
+    },
   ]
-  
+
 })
