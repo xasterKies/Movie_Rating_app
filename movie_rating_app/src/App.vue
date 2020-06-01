1a0728ce9ab48078fdc37e879129a7efeb8d5767
SamuelEdward <samueledward40@gmail.com>
2020-06-01 05:04:21 -0700

    editing template core file

--- movie_rating_app/src/App.vue
+++ movie_rating_app/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app id="inspire">
-  <v-navigation-drawer
- fixed
- v-model="drawer"
- app
- >
+    <v-navigation-drawer
+    fixed
+    v-model="drawer"
+    app
+    >
  <v-list dense>
  <router-link v-bind:to="{ name: 'Home' }" class="side_bar_link">
  <v-list-tile>
